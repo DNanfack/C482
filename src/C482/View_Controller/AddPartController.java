5ff88fab29b87929026e577f7012f8e8a7e28930
Taylor Vories <taylor@tmtech.me>
2018-07-14 19:46:43 -0600

    Added basic part controller functionality.  Defined all text fields.  Added cancel confirmation.

--- src/C482/View_Controller/AddPartController.java
+++ src/C482/View_Controller/AddPartController.java
@@ -1,31 +1,114 @@
 package C482.View_Controller;
 
 import C482.Main;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AddPartController implements Initializable {
     private Main main;
+    private boolean inHousePart;
+    @FXML
+    RadioButton inHouseRadio;
+    @FXML
+    RadioButton outsourcedRadio;
+    @FXML
+    ToggleGroup addPartRadioGroup;
+    @FXML
+    GridPane partGridPane;
+    @FXML
+    Label optionalRowLabel;
+    @FXML
+    TextField optionalRowTextfield;
+    @FXML
+    TextField partNameTextField;
+    @FXML
+    TextField inventoryTextField;
+    @FXML
+    TextField priceCostTextField;
+    @FXML
+    TextField maxTextField;
+    @FXML
+    TextField minTextField;
+
 
     public void setMain(Main main) {
         this.main = main;
     }
 
-    public void showMainScreen(ActionEvent event) throws IOException {
-        main.showMainScreen("parts");
+    public void showMainScreen() {
+        try {
+            main.showMainScreen("parts");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showInHouseField() {
+        optionalRowLabel.setText("Machine ID");
+        optionalRowLabel.setVisible(true);
+        optionalRowTextfield.setVisible(true);
+        inHousePart = true;
+    }
+    public void showOutsourcedField() {
+        optionalRowLabel.setText("Company Name");
+        optionalRowLabel.setVisible(true);
+        optionalRowTextfield.setVisible(true);
+        inHousePart = false;
+    }
+
+    public void showValuesAndChange() {
+        System.out.println("Values of fields:");
+        System.out.println("Part Name: " + partNameTextField.getText());
+        System.out.println("Inventory: " + inventoryTextField.getText());
+        System.out.println("Price/Cost: " + priceCostTextField.getText());
+        System.out.println("Max: " + maxTextField.getText());
+        System.out.println("Min: " + minTextField.getText());
+
+        showMainScreen();
+    }
+
+    public void cancelButtonPressed() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Cancel add part?");
+        alert.setHeaderText("Are you sure you want to cancel?");
+        alert.setContentText("Are you sure?");
+        Optional<ButtonType> optional = alert.showAndWait();
+        if(optional.get() == ButtonType.OK) {
+            showMainScreen();
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        optionalRowLabel.setVisible(false);
+        optionalRowTextfield.setVisible(false);
+        inHouseRadio.fire();
+        addPartRadioGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                if(addPartRadioGroup.selectedToggleProperty() != null) {
+                    if(inHouseRadio.isSelected()) {
+                        showInHouseField();
+                    } else if(outsourcedRadio.isSelected()) {
+                        showOutsourcedField();
+                    }
+                }
+            }
+        });
     }
 }
